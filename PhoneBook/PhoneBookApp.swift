907ee453f3437d01d5b7b4da6e6f5c07fa74dca1
Cihat Tasci <cihattasci97@gmail.com>
2023-09-06 16:27:39 +0300

    Initial Commit

---
+++ PhoneBook/PhoneBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneBookApp.swift
+//  PhoneBook
+//
+//  Created by Cihat Tascı on 6.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhoneBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
